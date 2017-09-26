dbe45555304f66bda8abd7a109df4fd19d9f760d
Luis Guilherme <lgdagostim@users.noreply.github.com>
2017-09-26 15:46:56 -0300

    ui updates

--- js/app.js
+++ js/app.js
@@ -3,18 +3,13 @@ var app = new Vue({
     data: {
         today: new Date().toLocaleDateString(),
         todos: [
-        { text: 'task 01 example' },
-        { text: 'task 02 example' },
-        { text: 'task 03 example' }
+            { text: 'task 01 example' },
+            { text: 'task 02 example' },
+            { text: 'task 03 example' }
         ],
         //hints on individuals task action buttons
         hint_complete: 'Mark this task as completed',
         hint_edit: 'Edit this task',
         hint_delete: 'Delete this task'
-    },
-    methods: {
-        add: function(){
-            this.todos.push({text: 'task'});
-        }
     }
 });
