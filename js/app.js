9d6a31a4ecc955bfcfe252776f3255d5c4b5ba91
Luis Guilherme <lgdagostim@users.noreply.github.com>
2017-09-26 22:02:02 -0300

    UI/UX updates

--- js/app.js
+++ js/app.js
@@ -7,9 +7,21 @@ var app = new Vue({
             { text: 'task 02 example' },
             { text: 'task 03 example' }
         ],
+        today_tasks: 3,
         //hints on individuals task action buttons
         hint_complete: 'Mark this task as completed',
         hint_edit: 'Edit this task',
         hint_delete: 'Delete this task'
     }
 });
+/*
+$(document).ready(function(){
+    $('#btn_menu').click(function(){
+        if ($('#menu_items').is(':visible')){
+            $('#menu_items').hide(1000);
+        }else{
+            $('#menu_items').show(1000);
+        }
+    })
+})
+*/
