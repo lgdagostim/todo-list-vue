ee3479aff4e975a4f47e3506cca5026e154dbb8d
Luis Guilherme <lgdagostim@users.noreply.github.com>
2017-09-27 21:21:08 -0300

    ui updates
    now tasks can be added by input text

--- js/app.js
+++ js/app.js
@@ -1,27 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
+        newTask: "",
         today: new Date().toLocaleDateString(),
-        todos: [
-            { text: 'task 01 example' },
-            { text: 'task 02 example' },
-            { text: 'task 03 example' }
-        ],
+        todos: [],
         today_tasks: 3,
         //hints on individuals task action buttons
         hint_complete: 'Mark this task as completed',
         hint_edit: 'Edit this task',
         hint_delete: 'Delete this task'
+    },
+    methods: {
+        addTask: function () {
+            var task = this.newTask.trim();
+            if (task) {
+                this.todos.push({
+                    text: task
+                });
+            }
+        }
     }
 });
-/*
-$(document).ready(function(){
-    $('#btn_menu').click(function(){
-        if ($('#menu_items').is(':visible')){
-            $('#menu_items').hide(1000);
-        }else{
-            $('#menu_items').show(1000);
-        }
-    })
-})
-*/
